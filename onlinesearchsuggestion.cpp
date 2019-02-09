1ad1d444d3f161214f5eae1d8c769d2ec73d4fc5
keshavbhatt <keshavnrj@gmail.com>
2019-02-09 17:21:32 +0530

    search now lists suggestions for queries

---
+++ onlinesearchsuggestion.cpp
@@ -0,0 +1,179 @@
+#include "onlinesearchsuggestion.h"
+
+const QString suggestUrl(QStringLiteral("http://suggestqueries.google.com/complete/search?ds=yt&client=youtube&hjson=t&cp=1&format=5&alt=json&q=%1"));
+
+
+onlineSearchSuggestion::onlineSearchSuggestion(QLineEdit *parent): QObject(parent), editor(parent)
+{
+    blacklist<<"whatsapp status"<<"full movie"<<"download"<<"movie";
+
+    popup = new QTreeWidget;
+    popup->setWindowFlags(Qt::Popup);
+    popup->setFocusPolicy(Qt::NoFocus);
+    popup->setFocusProxy(parent);
+    popup->setMouseTracking(true);
+
+    popup->setColumnCount(1);
+    popup->setUniformRowHeights(true);
+    popup->setRootIsDecorated(false);
+    popup->setEditTriggers(QTreeWidget::NoEditTriggers);
+    popup->setSelectionBehavior(QTreeWidget::SelectRows);
+    popup->setFrameStyle(QFrame::Box | QFrame::Plain);
+    popup->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    popup->header()->hide();
+    popup->setStyleSheet("border:none;");
+
+    popup->installEventFilter(this);
+
+    connect(popup, SIGNAL(itemClicked(QTreeWidgetItem*,int)),
+            SLOT(doneCompletion()));
+
+    timer.setSingleShot(true);
+    timer.setInterval(500);
+    connect(&timer, SIGNAL(timeout()), SLOT(autoSuggest()));
+    connect(editor, SIGNAL(textEdited(QString)), &timer, SLOT(start()));
+
+    connect(&networkManager, SIGNAL(finished(QNetworkReply*)),
+            this, SLOT(handleNetworkData(QNetworkReply*)));
+}
+
+
+onlineSearchSuggestion::~onlineSearchSuggestion()
+{
+    delete popup;
+}
+
+bool onlineSearchSuggestion::eventFilter(QObject *obj, QEvent *ev)
+{
+    if (obj != popup)
+        return false;
+
+    if (ev->type() == QEvent::MouseButtonPress) {
+        popup->hide();
+        editor->setFocus();
+        return true;
+    }
+
+    if (ev->type() == QEvent::KeyPress) {
+        bool consumed = false;
+        int key = static_cast<QKeyEvent*>(ev)->key();
+        switch (key) {
+        case Qt::Key_Enter:
+        case Qt::Key_Return:
+            doneCompletion();
+            consumed = true;
+            break;
+
+        case Qt::Key_Escape:
+            editor->setFocus();
+            popup->hide();
+            consumed = true;
+            break;
+
+        case Qt::Key_Up:
+        case Qt::Key_Down:
+        case Qt::Key_Home:
+        case Qt::Key_End:
+        case Qt::Key_PageUp:
+        case Qt::Key_PageDown:
+            break;
+
+        default:
+            editor->setFocus();
+            editor->event(ev);
+            popup->hide();
+            break;
+        }
+
+        return consumed;
+    }
+
+    return false;
+}
+
+void onlineSearchSuggestion::showCompletion(const QVector<QString> &choices)
+{
+    if (choices.isEmpty())
+        return;
+
+    const QPalette &pal = editor->palette();
+    QColor color = pal.color(QPalette::Normal, QPalette::WindowText);
+
+    popup->setUpdatesEnabled(false);
+    popup->clear();
+
+    for (const auto &choice : choices) {
+        auto item  = new QTreeWidgetItem(popup);
+        item->setText(0, choice);
+        item->setTextColor(0, color);
+    }
+
+    popup->setCurrentItem(popup->topLevelItem(0));
+    popup->resizeColumnToContents(0);
+    popup->setUpdatesEnabled(true);
+
+    popup->move(editor->mapToGlobal(QPoint(0, editor->height())));
+    popup->setFocus();
+    popup->show();
+}
+
+void onlineSearchSuggestion::doneCompletion()
+{
+    timer.stop();
+    popup->hide();
+    editor->setFocus();
+    QTreeWidgetItem *item = popup->currentItem();
+    if (item) {
+        editor->setText(item->text(0));
+        QMetaObject::invokeMethod(editor, "returnPressed");
+    }
+}
+
+void onlineSearchSuggestion::autoSuggest()
+{
+    QString str = editor->text();
+    QString url = suggestUrl.arg(str);
+    networkManager.get(QNetworkRequest(url));
+}
+
+void onlineSearchSuggestion::preventSuggest()
+{
+    timer.stop();
+}
+
+void onlineSearchSuggestion::handleNetworkData(QNetworkReply *networkReply)
+{
+//    QUrl url = networkReply->url();
+    if (networkReply->error() == QNetworkReply::NoError) {
+        QVector<QString> choices;
+
+        QByteArray response(networkReply->readAll());
+        QJsonDocument jsonResponse = QJsonDocument::fromJson(response);
+        QJsonArray json_array = jsonResponse.array();
+            foreach (const QJsonValue &value, json_array) {
+                if(value.isArray()){
+                     foreach (const QJsonValue &term, value.toArray()) {
+                         if(term.isArray()){
+                             foreach (const QJsonValue &val, term.toArray()) {
+                                 if(val.isString() && !val.toString().isEmpty() && !checkBlackList(val.toString())){
+                                     choices << val.toString();
+                                 }
+                             }
+                         }
+                     }
+                }
+            }
+        showCompletion(choices);
+    }
+    networkReply->deleteLater();
+}
+
+bool onlineSearchSuggestion::checkBlackList(QString word){
+    bool found = false;
+    foreach (const QString blacklisted, blacklist) {
+        if(word.contains(blacklisted,Qt::CaseInsensitive)){
+            found = true;
+        }
+    }
+    return found;
+}
---
+++ onlinesearchsuggestion.h
@@ -0,0 +1,47 @@
+#ifndef ONLINESEARCHSUGGESTION_H
+#define ONLINESEARCHSUGGESTION_H
+
+#include <QObject>
+#include <QLineEdit>
+#include <QTreeWidget>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QByteArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QTimer>
+#include <QHeaderView>
+#include <QEvent>
+#include <QJsonArray>
+#include <QKeyEvent>
+
+
+class onlineSearchSuggestion : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit onlineSearchSuggestion(QLineEdit *parent = nullptr);
+    ~onlineSearchSuggestion();
+    bool eventFilter(QObject *obj, QEvent *ev) override;
+    void showCompletion(const QVector<QString> &choices);
+
+public slots:
+
+    void doneCompletion();
+    void preventSuggest();
+    void autoSuggest();
+    void handleNetworkData(QNetworkReply *networkReply);
+
+private slots:
+    bool checkBlackList(QString word);
+private:
+    QLineEdit *editor = nullptr;
+    QTreeWidget *popup = nullptr;
+    QTimer timer;
+    QNetworkAccessManager networkManager;
+    QStringList blacklist;
+};
+
+#endif // ONLINESEARCHSUGGESTION_H
