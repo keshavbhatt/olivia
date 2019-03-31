deddbe2a548f0e4818cf5d1fa18e383b818e2617
keshavbhatt <keshavnrj@gmail.com>
2019-03-31 17:12:36 +0530

    clean up

--- web/radio/main.js
+++ web/radio/main.js
@@ -165,16 +165,6 @@ $(document).ready(function($) {
 });
 
 
-
-function capitalize(str) {
-    strVal = '';
-    str = str.split(' ');
-    for (var chr = 0; chr < str.length; chr++) {
-      strVal += str[chr].substring(0, 1).toUpperCase() + str[chr].substring(1, str[chr].length) + ' '
-    }
-    return strVal;
-}
-
 //  core functions -------------
 
 function loadCountries(){
