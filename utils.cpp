64ba20394c90050683d4d027e1ffcd608b75eca3
keshavbhatt <keshavnrj@gmail.com>
2019-05-29 20:10:09 +0530

    utils class

---
+++ utils.cpp
@@ -0,0 +1,63 @@
+#include "utils.h"
+
+utils::utils(QObject *parent) : QObject(parent)
+{
+    setParent(parent);
+}
+
+utils::~utils()
+{
+    this->deleteLater();
+}
+
+//calculate dir size
+quint64 utils::dir_size(const QString & directory)
+{
+    quint64 sizex = 0;
+    QFileInfo str_info(directory);
+    if (str_info.isDir())
+    {
+        QDir dir(directory);
+        QFileInfoList list = dir.entryInfoList(QDir::Files | QDir::Dirs |  QDir::Hidden | QDir::NoSymLinks | QDir::NoDotAndDotDot);
+        for (int i = 0; i < list.size(); ++i)
+        {
+            QFileInfo fileInfo = list.at(i);
+            if(fileInfo.isDir())
+            {
+                sizex += dir_size(fileInfo.absoluteFilePath());
+            }
+            else
+            sizex += fileInfo.size();
+        }
+    }
+    return sizex;
+}
+
+//get the size of cache folder in human readble format
+QString utils::refreshCacheSize(const QString cache_dir)
+{
+  //  QString cache_dir =  QStandardPaths::writableLocation(QStandardPaths::DataLocation)+"/data8";
+    int cache_size = dir_size(cache_dir);
+    QString cache_unit;
+    if(cache_size > 1024*1024*1024)
+    {
+        cache_size = cache_size/(1024*1024*1024);
+        cache_unit = " GB";
+    }
+    if(cache_size > 1024*1024)
+    {
+        cache_size = cache_size/(1024*1024);
+        cache_unit = " MB";
+    }
+    else if(cache_size > 1024)
+    {
+        cache_size = cache_size/(1024);
+        cache_unit = " kB";
+    }
+    else
+    {
+        cache_size = cache_size;
+        cache_unit = " B";
+    }
+    return QString::number(cache_size) + cache_unit;
+}
---
+++ utils.h
@@ -0,0 +1,24 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <QObject>
+#include <QDebug>
+#include <QFileInfo>
+#include <QStandardPaths>
+#include <QDir>
+
+class utils : public QObject
+{
+    Q_OBJECT
+
+public:
+    utils(QObject* parent=0);
+    virtual ~utils();
+public slots:
+    QString refreshCacheSize(const QString cache_dir);
+private slots:
+    quint64 dir_size(const QString &directory);
+
+};
+
+#endif // UTILS_H
