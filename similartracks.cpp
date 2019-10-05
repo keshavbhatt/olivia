94e9379e0499ba23b3f46b8b399fb37a455f0a5a
keshavbhatt <keshavnrj@gmail.com>
2019-10-05 15:35:32 +0530

    prevent app crash if last track in queue is playing and smart playlist got no new tracks to play..

--- similartracks.cpp
+++ similartracks.cpp
@@ -19,22 +19,24 @@ void SimilarTracks::addSimilarTracks(QString video_id){
             foreach (QString str, list) {
                finalList.append(QString(str.split(");\">").first()).remove("&quot;"));
             }
-            for (int i = 0; i < 5;/*finalList.count();*/ i++) {
-                QString videoId,title,artist,album,coverUrl,songId,albumId,artistId,millis;
-                QStringList arr = QString(finalList.at(i)).split("!=-=!");
-                title = arr[0];
-                artist = arr[1];
-                album = arr[2];
-                coverUrl = arr[3];
-                songId = arr[4];
-                albumId = arr[5];
-                artistId= arr[6];
-                millis = arr[7];
+            if(finalList.count()>0){
+                for (int i = 0; i < 5;/*finalList.count();*/ i++) {
+                    QString videoId,title,artist,album,coverUrl,songId,albumId,artistId,millis;
+                    QStringList arr = QString(finalList.at(i)).split("!=-=!");
+                    title = arr[0];
+                    artist = arr[1];
+                    album = arr[2];
+                    coverUrl = arr[3];
+                    songId = arr[4];
+                    albumId = arr[5];
+                    artistId= arr[6];
+                    millis = arr[7];
 
-                if(albumId.contains("undefined")){ //yt case
-                    videoId = arr[4];
+                    if(albumId.contains("undefined")){ //yt case
+                        videoId = arr[4];
+                    }
+                    emit addToSimilarTracksList(QStringList()<<videoId<<title<<artist<<album<<coverUrl<<songId<<albumId<<artistId);
                 }
-                emit addToSimilarTracksList(QStringList()<<videoId<<title<<artist<<album<<coverUrl<<songId<<albumId<<artistId);
             }
         }
         rep->deleteLater();
