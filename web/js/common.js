2604c5d36b87aff6d0f87633ae564680c2a64191
keshavbhatt <keshavnrj@gmail.com>
2019-12-02 02:13:44 +0530

    common.js added isElementInViewport and timeAgo helper functions

--- web/js/common.js
+++ web/js/common.js
@@ -15,6 +15,77 @@ if (!String.prototype.includes) {
     };
 }
 
+function isElementInViewport (el) {
+    //special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+
+//usages = TimeAgo.inWords(date);
+var TimeAgo = (function() {
+  var self = {};
+  self.locales = {
+    prefix: '',
+    sufix:  'ago',
+
+    seconds: 'few seconds',
+    minute:  'a minute',
+    minutes: '%d minutes',
+    hour:    'an hour',
+    hours:   'about %d hours',
+    day:     'a day',
+    days:    '%d days',
+    month:   'about a month',
+    months:  '%d months',
+    year:    'about a year',
+    years:   '%d years'
+  };
+
+  self.inWords = function(timeAgo) {
+    var seconds = Math.floor((new Date() - parseInt(timeAgo)) / 1000),
+        separator = this.locales.separator || ' ',
+        words = this.locales.prefix + separator,
+        interval = 0,
+        intervals = {
+          year:   seconds / 31536000,
+          month:  seconds / 2592000,
+          day:    seconds / 86400,
+          hour:   seconds / 3600,
+          minute: seconds / 60
+        };
+
+    var distance = this.locales.seconds;
+
+    for (var key in intervals) {
+      interval = Math.floor(intervals[key]);
+
+      if (interval > 1) {
+        distance = this.locales[key + 's'];
+        break;
+      } else if (interval === 1) {
+        distance = this.locales[key];
+        break;
+      }
+    }
+
+    distance = distance.replace(/%d/i, interval);
+    words += distance + separator + this.locales.sufix;
+
+    return words.trim();
+  };
+
+  return self;
+}());
+
+
 function isEmpty(obj) {
     for(var key in obj) {
         if(obj.hasOwnProperty(key))
