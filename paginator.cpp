dabbf6560eafd40b289e493d1acd255554b794fe
keshavbhatt <keshavnrj@gmail.com>
2019-12-02 02:12:24 +0530

    paginator class now shows page saved in 'TimeAgo' format

--- paginator.cpp
+++ paginator.cpp
@@ -23,13 +23,14 @@ void paginator::save(QString pageType,QString dataType,QString query,QString dat
     if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
               return;
     QTextStream out(&file);
-    QString loadOnlineBtn;
-    if(pageType=="album_view"){
-        loadOnlineBtn  = "<a style='font-size: 0.88em; padding-top: 10px;margin: 10px 0px;background-color: rgba(36, 142, 179, 0.66);border: none;' class='ui-shadow-icon ui-btn ui-shadow ui-corner-all ui-icon-clock ui-btn-icon-left'  onclick='paginator.deleteCache(\""+pageType+"\",\""+dataType+"\",\""+query+"\")'> Saved on "+QDateTime::currentDateTime().toLocalTime().toString()+" - Reload</a>";
-    }else{
-        loadOnlineBtn  = "<a style='font-size: 0.88em; padding-top: 10px;margin: 10px 0px;background-color: rgba(36, 142, 179, 0.66);border: none;' class='ui-shadow-icon ui-btn ui-shadow ui-corner-all ui-icon-clock ui-btn-icon-left'   onclick='paginator.deleteCache(\""+pageType+"\",\""+dataType+"\",\""+query+"\")'> Saved on "+QDateTime::currentDateTime().toLocalTime().toString()+" - Reload</a>";
-    }
-    out <<loadOnlineBtn<< data.trimmed();
+    QString loadOnlineBtn = "<a id='timeElement"+QString::number(QDateTime::currentMSecsSinceEpoch())+"' style='font-size: 0.88em; padding-top: 10px;margin: 10px 0px;"
+                            "background-color: rgba(36, 142, 179, 0.66);border: none;'"
+                            " class='ui-shadow-icon ui-btn ui-shadow ui-corner-all ui-icon-clock ui-btn-icon-left' "
+                            " onclick='paginator.deleteCache(\""+pageType+"\",\""+dataType+"\",\""+query+"\")'> Saved on "+QDateTime::currentDateTime().toLocalTime().toString()+" - Reload</a>";
+
+    QString timeagoScript = "<script>var date = Date.parse('"+QDateTime::currentDateTime().toLocalTime().toString()+"');"
+                            "$.mobile.activePage.find('#timeElement"+QString::number(QDateTime::currentMSecsSinceEpoch())+"').text('Saved '+TimeAgo.inWords(date)+' - Reload');</script>";
+    out <<loadOnlineBtn<< data.trimmed()+timeagoScript;
 }
 
 //used to load data from offline page pageType for funcction dataType for query query
