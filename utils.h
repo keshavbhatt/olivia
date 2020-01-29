e89d3f169a0143be3e3a3705933d9cb10632034f
keshavbhatt <keshavnrj@gmail.com>
2020-01-29 19:19:17 +0530

    XMLencode styled html escaper added to utils

--- utils.h
+++ utils.h
@@ -17,8 +17,55 @@ public:
 public slots:
     QString refreshCacheSize(const QString cache_dir);
     bool delete_cache(const QString cache_dir);
-    QString toCamelCase(const QString &s);
+    static QString toCamelCase(const QString &s);
     static QString generateRandomId(int length);
+
+    static QString EncodeXML ( const QString& encodeMe ){
+
+        QString temp;
+        int length = encodeMe.size();
+        for (int index = 0; index < length; index++)
+        {
+            QChar character(encodeMe.at(index));
+
+            switch (character.unicode())
+            {
+            case '&':
+                temp += "&amp;"; break;
+
+            case '\'':
+                temp += "&apos;"; break;
+
+            case '"':
+                temp += "&quot;"; break;
+
+            case '<':
+                temp += "&lt;"; break;
+
+            case '>':
+                temp += "&gt;"; break;
+
+            default:
+                temp += character;
+                break;
+            }
+        }
+
+        return temp;
+    }
+
+    static QString DecodeXML ( const QString& decodeMe ) {
+
+        QString temp(decodeMe);
+
+        temp.replace("&amp;", "&");
+        temp.replace("&apos;", "'");
+        temp.replace("&quot;", "\"");
+        temp.replace("&lt;", "<");
+        temp.replace("&gt;", ">");
+
+        return temp;
+    }
 private slots:
     quint64 dir_size(const QString &directory);
 
